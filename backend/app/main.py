6cf6dcb8e6f36df0dfdda19c8caeb9b6dc266bf1
Subin Bista <subin.bista@selu.edu>
2025-10-03 23:20:18 -0400

    Added Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title= "Hackathon")
+
+# Adding the Cors MiddleWare
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins =["*"],
+    allow_methods = ["*"],
+    allow_credentials = True,
+    allow_headers = ["*"]
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
