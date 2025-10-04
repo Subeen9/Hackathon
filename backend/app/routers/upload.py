cd381603210936f67d51cdc491bba5b694ea8de5
pathak-satyam <satyampathak8059@gmail.com>
2025-10-04 02:21:21 -0500

    Preprocess the uploaded file

--- backend/app/routers/upload.py
+++ backend/app/routers/upload.py
@@ -1,24 +1,28 @@
 from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import FileResponse, JSONResponse
 from pathlib import Path
 import shutil
-from fastapi.responses import FileResponse
+from app.preprocess import preprocess_image  
 
 router = APIRouter()
-Upload_Dir = Path("uploads")
-Upload_Dir.mkdir(exist_ok=True)
-
-@router.get("/files/{filename}")
-async def get_file(filename: str):
-    file_path = Upload_Dir / filename
-    if not file_path.exists():
-        return {"error": "File not found"}
-    return FileResponse(file_path)
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
 
 @router.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    file_path = Upload_Dir / file.filename
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {"filename": file.filename, "url": f"http://localhost:8000/files/{file.filename}"}
+async def upload_and_preprocess(file: UploadFile = File(...)):
+    try:
+        file_path = UPLOAD_DIR / file.filename
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
+        # Run preprocessing
+        output_path = preprocess_image(str(file_path))
 
+        return FileResponse(
+            output_path,
+            media_type="image/png",
+            filename="preprocessed_clean.png"
+        )
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
