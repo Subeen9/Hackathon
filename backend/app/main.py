44ba018e5aeac9d1f075737b4f7a6dcd9d5f165b
Subin Bista <subin.bista@selu.edu>
2025-10-03 23:49:59 -0400

    Added Upload Endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routers import upload
 
-app = FastAPI(title= "Hackathon")
+app = FastAPI(title="Hackathon")
 
-# Adding the Cors MiddleWare
 app.add_middleware(
     CORSMiddleware,
-    allow_origins =["*"],
-    allow_methods = ["*"],
-    allow_credentials = True,
-    allow_headers = ["*"]
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
 )
 
+@app.get("/")
+async def root():
+    return {"message": "Hackathon API is running 🚀"}
 
+app.include_router(upload.router)
